1af328e1fa6df2ebc34b8b2ac5c796006749475c
Asalle <asalle.kim@gmail.com>
2016-01-26 13:30:51 +0100

    session manager documentation

--- sessionmanager.h
+++ sessionmanager.h
@@ -18,13 +18,18 @@ class SessionManager
 {
     SessionManager();
 
-    LoginPassDialog * dlg;
+    LoginPassDialog * dlg; /*!< login-pass-prompt-dialog pointer*/
 public:
+    /*!< name of session file*/
     static QString sharedClipboardIdFileName;
+    /*!< path to session file
+     * together withh session file name composes into
+     * full path to session file*/
     static QString idFilePath;
 
-    static bool sessionActive();
-    static bool sessionInterrupted();
+    static bool sessionActive(); /*!< if session is active flag*/
+    static bool sessionInterrupted(); /*!< if session was interrupted flag*/
+    /*!< start a session -- create session file*/
     void startSession( QSharedPointer<RoomService> & roomService,
                             QSharedPointer<UdpService> & udpService,
                             QSharedPointer<TcpService> & tcpService,
@@ -32,12 +37,12 @@ public:
                             QSharedPointer<FileService> & fileService,
                             QSharedPointer<EncryptionService> & encService);
 
-    static bool idFileExists();
-    static bool idFileOpenedForWriting();
+    static bool idFileExists(); /*!< if file exists flag*/
+    static bool idFileOpenedForWriting(); /*!< is file opened for writing flag*/
 
-    static QString getLogin(QFile source);
-    static QString setLogin(QString login);
-    static SessionManager * getInstance();    
+    static QString getLogin(QFile source); //wtf
+    static QString setLogin(QString login);//wtf
+    static SessionManager * getInstance(); /*!< singleton getInstance*/
 };
 
 
