63d54b17f7632859ecc1fc7b5e4381157b3d0ab7
Asalle <asalle.kim@gmail.com>
2016-01-26 13:22:42 +0100

    roomservice documentation

--- roomservice.h
+++ roomservice.h
@@ -12,31 +12,34 @@
 #include "tcpservice.h"
 #include "shared_clipboard_exception.h" //###
 
+//! Provides member management and is responsible
+//! for network services for members
 
 class RoomService : public QObject
 {
     Q_OBJECT
 
-    QSharedPointer<UdpService> udpService;
-    QSharedPointer<TcpService> tcpService;
+    QSharedPointer<UdpService> udpService; /*!< udpservice of me*/
+    QSharedPointer<TcpService> tcpService; /*!< tcpservice of me*/
 
-    QList<RoomMember> roomMembers;
-    QList<QString> roomsNames;
+    QList<RoomMember> roomMembers; /*!< list of my room members*/
+    QList<QString> roomsNames; /*!< names of rooms avaliable*/
 
-    QString ownRoomName;
-    QTimer * checkAlivesTimer;
+    QString ownRoomName; /*!< name of my room*/
+    QTimer * checkAlivesTimer; /*!< timer for checking alives*/
 public:
     explicit RoomService(QObject *parent = 0);
-    QList<QString> getRooms();
-    QList<RoomMember> getRoomMembers() const;
+    QList<QString> getRooms(); /*!< get list of available rooms*/
+    QList<RoomMember> getRoomMembers() const; /*!< get list of my room members*/
 
 signals:
     void refreshMembers(QList<RoomMember>);
 
 public slots:
+    /*!< add new member to members list*/
     void addMember(QString login, QString room, QList<QHostAddress> ip);
-    void checkAlives();
-    void setRoom(QString &);
+    void checkAlives(); /*!< checks if all my room members are alive*/
+    void setRoom(QString &); /*!< sets my room from the room list*/
 };
 
 #endif // CONTROL_H
