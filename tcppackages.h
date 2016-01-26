ebeffa29072cef18938648341153b89a7529ba38
Asalle <asalle.kim@gmail.com>
2016-01-26 13:33:46 +0100

    tcppackages documentation

--- tcppackages.h
+++ tcppackages.h
@@ -1,13 +1,14 @@
 #ifndef TCPPACKAGES
 #define TCPPACKAGES
 
+//! represents packages types
 enum class TcpPackage : char
 {
-    TXT,
-    PNG,
-    FILE_NOTIF,
-    FILE_REQ,
-    FILE_RESP,
+    TXT, /*!< plain text*/
+    PNG, /*!< png image*/
+    FILE_NOTIF, /*!< notification about avaliable file*/
+    FILE_REQ, /*!< request of file*/
+    FILE_RESP, /*!< file as response to file request*/
 };
 
 
